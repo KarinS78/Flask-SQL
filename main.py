e6b221c4d0533e36e8cbc7cd6aadac2d80c31af0
karin <karin.schwartz@gmail.io>
2022-06-29 16:44:31 +0300

    added a function to main.py (logout), and some html templates

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from crypt import methods
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, redirect, render_template, request, url_for, session, flash
+from datetime import timedelta
+
 app = Flask(__name__)
+app.secret_key = "hey Gabi"
+app.permanent_session_lifetime = timedelta(minutes=5)
 
 @app.route('/home')
 def hello_world():
@@ -9,21 +12,34 @@ def hello_world():
 @app.route("/login", methods=["POST","GET"])
 def login():
     if request.method == "POST":
-        usr = request.form["nm"]
-        return redirect(url_for("name", usr = name))
+        session.permanent = True
+        name = request.form["nm"]
+        session["name"] = name
+        flash("Login Succcesfull!")
+        return redirect(url_for("name"))
     else:
+        if "name" in session:
+            flash("Already Logged In!")
+            return redirect(url_for("name"))
+
         return render_template("login.html")
 
-# @app.route('/<name>')
-# def name(name):
-#     if name == "gabi":
-#         return redirect('/home')
-#     elif name == "login":
-#         return redirect(url_for("login"))
-#     else:
-#         return f"hello <h1>{name}</h1> :)"
+@app.route('/name')
+def name():
+    if "name" in session:
+        user = session["name"]
+        return f"hello <h1>{name}</h1> :)"
+    else: 
+        flash("Already Logged In!")
+        return  render_template("user.html", name = name)
 
-
+@app.route("/logout")
+def logout():
+    if "name" in session:
+        user = session["name"]
+        flash("you have been logged out!", "info")
+    session.pop("user", None)
+    return redirect(url_for("login"))
 
 
 if __name__ == '__main__':
