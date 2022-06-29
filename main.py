e35b7fcede297a611ee04aa66bfbbd5fefd7c96c
karin <karin.schwartz@gmail.io>
2022-06-29 14:51:48 +0300

    added more functions to main

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-from flask import Flask, redirect, render_template
+from crypt import methods
+from flask import Flask, redirect, render_template, request, url_for
 app = Flask(__name__)
 
 @app.route('/home')
 def hello_world():
     return render_template('home.html')
 
-@app.route('/<name>')
-def user(name):
-    if name == "gabi":
-        return redirect('/home')
+@app.route("/login", methods=["POST","GET"])
+def login():
+    if request.method == "POST":
+        usr = request.form["nm"]
+        return redirect(url_for("name", usr = name))
     else:
-        return f"hello {name} :)"
+        return render_template("login.html")
+
+# @app.route('/<name>')
+# def name(name):
+#     if name == "gabi":
+#         return redirect('/home')
+#     elif name == "login":
+#         return redirect(url_for("login"))
+#     else:
+#         return f"hello <h1>{name}</h1> :)"
+
+
+
 
 if __name__ == '__main__':
     app.run()
